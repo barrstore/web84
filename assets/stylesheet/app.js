30c91b9f697222b43d98ed9c2d94c5c106fc5552
barrstore <154892634+barrstore@users.noreply.github.com>
2024-07-01 16:35:11 +0700

    update assets/stylesheet/app.js

--- assets/stylesheet/app.js
+++ assets/stylesheet/app.js
@@ -6,21 +6,4 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("scrolled");
   }
-  const toastContainer = document.querySelector(".toast-container");
-const closeBtn = document.querySelector(".toast-container .close");
-const toastLink = document.querySelector(".toast-container a");
-
-if (!localStorage.getItem("displayToast")) {
-  setTimeout(() => {
-    toastContainer.classList.add("active");
-  }, 100);
-}
-
-const stopDisplayingToast = () => {
-  localStorage.setItem("displayToast", false);
-  toastContainer.classList.remove("active");
-};
-
-closeBtn.addEventListener("click", stopDisplayingToast);
-toastLink.addEventListener("click", stopDisplayingToast);
 });
