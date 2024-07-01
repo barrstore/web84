75498383c6d8c8bb00247b7836f4ac0ec5c678a3
barrstore <154892634+barrstore@users.noreply.github.com>
2024-07-01 16:27:37 +0700

    update assets/stylesheet/app.js

--- assets/stylesheet/app.js
+++ assets/stylesheet/app.js
@@ -6,4 +6,21 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("scrolled");
   }
+  const toastContainer = document.querySelector(".toast-container");
+const closeBtn = document.querySelector(".toast-container .close");
+const toastLink = document.querySelector(".toast-container a");
+
+if (!localStorage.getItem("displayToast")) {
+  setTimeout(() => {
+    toastContainer.classList.add("active");
+  }, 1000);
+}
+
+const stopDisplayingToast = () => {
+  localStorage.setItem("displayToast", false);
+  toastContainer.classList.remove("active");
+};
+
+closeBtn.addEventListener("click", stopDisplayingToast);
+toastLink.addEventListener("click", stopDisplayingToast);
 });
