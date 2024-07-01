38fe6adbee8ac3d81c61927000abd7a2d6ad9f0b
barrstore <154892634+barrstore@users.noreply.github.com>
2024-07-01 16:06:02 +0700

    update assets/stylesheet/app.js

--- assets/stylesheet/app.js
+++ assets/stylesheet/app.js
@@ -7,3 +7,15 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+document.querySelector(".close").addEventListener("click" , function(){
+    document.querySelector(".notif").style.display = "none";
+});
+
+window.addEventListener("load" , function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".notif").style.display = "block";
+        },
+        2000
+    )
+});
